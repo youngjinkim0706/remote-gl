71290a72d9539ce2d474c0c8c6a6c6e2aa3d02b2
yjkim <play1490@naver.com>
2021-09-22 02:10:11 +0900

    change bind to connect

--- glremote_server/glremote_server.cpp
+++ glremote_server/glremote_server.cpp
@@ -40,7 +40,7 @@ void Server::server_bind()
 {
     sock = zmq::socket_t(ctx, zmq::socket_type::pair);
 
-    sock.bind("tcp://" + ip_address + ":" + port);
+    sock.connect("tcp://" + ip_address + ":" + port);
 
     if (enableStreaming)
     {
